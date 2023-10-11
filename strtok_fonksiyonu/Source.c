bc7e70ce4047cf885e9dd8348f07af3a637f7723
ercankacmaz <ercankacmaz870870@gmail.com>
2023-10-11 16:15:39 +0300

    strtok_fonksiyonu

---
+++ strtok_fonksiyonu/Source.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+
+/*
+strtok fonksiyonu  stringleri tokenlara ayırmak için kuyllanılır. <string.h> kütüphanesinde bulunur.
+Prototipi  char *strtok(char *str, const char *delin)
+*str tokenlarına ayrılacak olan dizinin adresini argüman olarak alır, *delin ise stringteki ayırmaları yapacak karakteri argüman olarak alır. 
+Bir döngü ile kullanılır. Eğer fonksiyon sadece bir kez çağırılırsa sadece ilk ayrılan kısım yazdırılabilir. 
+*/
+
+
+int main()
+{
+	char s[] = "ali,veli,hakan";
+	char* str;
+
+	for (str = strtok(s, ","); str != NULL; str = strtok(NULL, ","))
+	{
+		puts(str);
+	}
+	return 0;
+
+}
